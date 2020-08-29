5f2cee37ebd1ef03e28d6bfd48d3302b8ec24961
ChingChieh <garnett.huang@gmail.com>
2020-08-29 15:23:24 -0400

    Initialize project with Firebase install

---
+++ ToDoList/ToDoList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by ChingChieh on 2020/8/29.
+//  Copyright © 2020 ChingChieh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
