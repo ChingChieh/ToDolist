b7ac0139d9bfce356aed0bed40eff271eeb004bc
ChingChieh <garnett.huang@gmail.com>
2020-08-29 16:17:16 -0400

    Able to add users to my database

--- ToDoList/ToDoList/ViewController.swift
+++ ToDoList/ToDoList/ViewController.swift
@@ -7,14 +7,45 @@
 //
 
 import UIKit
+import FirebaseDatabase
+import FirebaseAuth
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var emailTF: UITextField!
+    @IBOutlet weak var passwordTF: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func registerAction(_ sender: Any) {
+        if (emailTF.text != nil) && (passwordTF.text != nil) {
+            Auth.auth().createUser(withEmail: emailTF.text!, password: passwordTF.text!){ (result, error) in
+                if error != nil{
+                    print("Register Error")
+                } else {
+                    let uid = result?.user.uid
+                    let ref = Database.database().reference(withPath: "users").child(uid!)
+                    ref.setValue(["email":self.emailTF.text!, "password":self.passwordTF.text!])
+                }
+            }
+        }
+    }
+    
+    @IBAction func signInAction(_ sender: Any) {
+        if (emailTF.text != nil) && (passwordTF.text != nil) {
+            Auth.auth().signIn(withEmail: emailTF.text!, password: passwordTF.text!){ (result, error) in
+                if error != nil{
+                    print("Signin Error")
+                } else {
+                    //let uid = result?.user.uid
+                
+                }
+            }
+        }
+    }
+    
 }
 
